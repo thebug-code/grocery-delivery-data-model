c982658230ee7e6a6a55fcdde95796d7c91bf21c
admeneses <alejandrocardie12@gmail.com>
2023-04-13 16:37:16 -0400

    us_cities anadido

--- generate_data.py
+++ generate_data.py
@@ -6,6 +6,79 @@ import csv
 # Initialize Faker with 'en_US' locale
 fake = Faker('en_US')
 
+# Obtiene los nombres de las 20 ciudades mas pobladas de Estados Unidos
+cities = [
+    'New York',
+    'Los Angeles',
+    'Chicago',
+    'Houston',
+    'Phoenix',
+    'Philadelphia',
+    'San Antonio',
+    'San Diego',
+    'Dallas',
+    'San Jose',
+    'Austin',
+    'Fort Worth',
+    'Jacksonville',
+    'Columbus',
+    'San Francisco',
+    'Charlotte',
+    'Indianapolis',
+    'Seattle',
+    'Denver',
+    'Washington'
+]
+
+# Define la poblacion de las 20 ciudades mas pobladas de Estados Unidos
+# (fuente: US Census Bureau, 2020)
+populations = [
+    8336817,
+    3970219,
+    2693976,
+    2320268,
+    1680992,
+    1584064,
+    1550864,
+    1423851,
+    1343573, 
+    1035317,
+    1002668,
+    927720,
+    911507,
+    898553,
+    883305,
+    875538,
+    876384,
+    769714,
+    727211,
+    693972
+]
+
+# Define los estados de las 20 ciudades mas pobladas de Estados Unidos
+states = [
+    'NY',
+    'CA',
+    'IL',
+    'TX',
+    'AZ',
+    'PA',
+    'TX',
+    'CA',
+    'TX',
+    'CA',
+    'TX',
+    'TX',
+    'FL',
+    'OH',
+    'CA',
+    'NC',
+    'IN',
+    'WA',
+    'CO',
+    'DC'
+]
+
 # Get the base directory where this Python script is located
 basedir = os.path.abspath(os.path.dirname(__file__))
 
@@ -14,79 +87,6 @@ def generate_address():
     Genera direciones aleatorias que corresponden a las 20 ciudades mas
     pobladas de Estados Unidos y las guarda en un archivo CSV
     """
-    # Obtiene los nombres de las 20 ciudades mas pobladas de Estados Unidos
-    cities = [
-        'New York',
-        'Los Angeles',
-        'Chicago',
-        'Houston',
-        'Phoenix',
-        'Philadelphia',
-        'San Antonio',
-        'San Diego',
-        'Dallas',
-        'San Jose',
-        'Austin',
-        'Fort Worth',
-        'Jacksonville',
-        'Columbus',
-        'San Francisco',
-        'Charlotte',
-        'Indianapolis',
-        'Seattle',
-        'Denver',
-        'Washington'
-    ]
-    
-    # Define la poblacion de las 20 ciudades mas pobladas de Estados Unidos
-    # (fuente: US Census Bureau, 2020)
-    populations = [
-        8336817,
-        3970219,
-        2693976,
-        2320268,
-        1680992,
-        1584064,
-        1550864,
-        1423851,
-        1343573, 
-        1035317,
-        1002668,
-        927720,
-        911507,
-        898553,
-        883305,
-        875538,
-        876384,
-        769714,
-        727211,
-        693972
-    ]
-    
-    # Define los estados de las 20 ciudades mas pobladas de Estados Unidos
-    states = [
-        'NY',
-        'CA',
-        'IL',
-        'TX',
-        'AZ',
-        'PA',
-        'TX',
-        'CA',
-        'TX',
-        'CA',
-        'TX',
-        'TX',
-        'FL',
-        'OH',
-        'CA',
-        'NC',
-        'IN',
-        'WA',
-        'CO',
-        'DC'
-    ]
-
     # Calcula la poblacion total
     total_population = sum(populations)
 
@@ -140,5 +140,35 @@ def generate_address():
                 # Escribe la direccion en el archivo CSV
                 writer.writerow([street_address, city_name, state, zip_code])
 
+def build_cvs_city():
+    """
+    Contruye el archivo CSV con las 20 ciudades mas pobladas de Estados Unidos
+    """
+    # Encabezados del archivo CSV
+    headers = ['City', 'State', 'Population']
+
+    # Este archivo se guardara en la misma carpeta que este script
+    file_path = os.path.join(basedir, 'us_cities.csv')
+    
+    # Abre un nuevo archivo CSV en modo de escritura y escribe los encabezados
+    with open(file_path, 'w', newline='') as f:
+        writer = csv.writer(f)
+        writer.writerow(headers)
+
+        for i, city in enumerate(cities):
+            # Obtiene el nombre de la ciudad de la lista de ciudades
+            city_name = city
+
+            # Generate a random state abbreviation
+            state = states[i]
+
+            # Obtiene la poblacion de la ciudad de la lista de poblaciones
+            population = populations[i]
+
+            # Escribe la direccion en el archivo CSV
+            writer.writerow([city_name, state, population])
+        
+
 if __name__ == '__main__':
-    generate_address()
+    #generate_address()
+    build_cvs_city()
