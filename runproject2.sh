a7e23526bfca1fe3d150c539bea56126831823da
admeneses <alejandrocardie12@gmail.com>
2023-04-15 01:15:16 -0400

    added llamar a sp

--- runproject2.sh
+++ runproject2.sh
@@ -23,6 +23,31 @@ while [ -z "$password" ]
 do
     read -sp "La contraseña no puede estar vacía. Ingrese la contraseña de PostgreSQL: " password
 done
+echo ""
+
+read -p "Ingrese el número de clientes a generar: " number_of_customers
+while [ -z "$number_of_customers" ]
+do
+    read -p "El número de clientes no puede estar vacío. Ingrese el número de clientes a generar: " number_of_customers
+done
+
+read -p "Ingrese el numero de ordenes a generar: " number_of_orders
+while [ -z "$number_of_orders" ]
+do
+    read -p "El número de ordenes no puede estar vacío. Ingrese el número de ordenes a generar: " number_of_orders
+done
+
+read -p "Ingrese el numero de productos a generar: " number_of_items
+while [ -z "$number_of_items" ]
+do
+    read -p "El número de productos no puede estar vacío. Ingrese el número de productos a generar: " number_of_items 
+done
+
+read -p "Ingrese el promedio de productos por orden: " avg_items_per_order
+while [ -z "$avg_items_per_order" ]
+do
+    read -p "El promedio de productos no puede estar vacío. Ingrese el promedio de productos por orden: " promedio_productos
+done
 
 # Configura las variables de entorno
 port='5432'
@@ -43,6 +68,13 @@ psql \
       -U "$user" \
       -d "$database" \
       -a -f "base_tables.sql"
+
+psql \
+      -h "$host" \
+      -p "$port" \
+      -U "$user" \
+      -d "$database" \
+      -a -f "tables_grocery_delivery.sql"
 
 psql \
       -h "$host" \
@@ -98,14 +130,22 @@ psql \
      -p "$port" \
      -U "$user" \
      -d "$database" \
-     -c "\\copy product_prices(price) FROM 'us_data/product_prices.csv' DELIMITER ',' CSV HEADER;"
+     -c "\\copy product_image_urls(image_url) FROM 'us_data/product_image_urls.csv' DELIMITER ',' CSV HEADER;"
 
 psql \
      -h "$host" \
      -p "$port" \
      -U "$user" \
      -d "$database" \
-     -c "\\copy product_image_urls(image_url) FROM 'us_data/product_image_urls.csv' DELIMITER ',' CSV HEADER;"
+     -f "spCreateTestData.sql"
+
+# Llama a la función de carga de datos
+psql \
+      -h "$host" \
+      -p "$port" \
+      -U "$user" \
+      -d "$database" \
+      -c "SELECT spCreateTestData($number_of_customers, $number_of_orders, $number_of_items, $avg_items_per_order);"
 
 # Inicia el servidor
 psql \
